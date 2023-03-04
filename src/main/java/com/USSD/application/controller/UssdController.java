2ebcf93672ad9540280f8a2944e1cef9b71c98d7
slydesigns <sylvestorsielei@gmail.com>
2023-03-04 20:33:32 +0300

    request method

--- src/main/java/com/USSD/application/controller/UssdController.java
+++ src/main/java/com/USSD/application/controller/UssdController.java
@@ -3,6 +3,7 @@ package com.USSD.application.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class UssdController {
