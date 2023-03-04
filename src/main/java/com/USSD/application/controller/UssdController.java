f2b747bad752fdba00d3503e5c70cad6e2d16b80
slydesigns <sylvestorsielei@gmail.com>
2023-03-04 20:24:24 +0300

    request param

--- src/main/java/com/USSD/application/controller/UssdController.java
+++ src/main/java/com/USSD/application/controller/UssdController.java
@@ -1,5 +1,6 @@
 package com.USSD.application.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
