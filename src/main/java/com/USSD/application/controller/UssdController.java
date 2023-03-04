99b7e5cfdcda1f11741e28f7dfc14e40b6aef3c8
slydesigns <sylvestorsielei@gmail.com>
2023-03-04 20:32:39 +0300

    some errors

--- src/main/java/com/USSD/application/controller/UssdController.java
+++ src/main/java/com/USSD/application/controller/UssdController.java
@@ -2,6 +2,7 @@ package com.USSD.application.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class UssdController {
