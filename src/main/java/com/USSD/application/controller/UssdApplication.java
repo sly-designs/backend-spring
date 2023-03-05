472a4d1adcd24b55a9ec87cdc68ad4fb07c00743
slydesigns <sylvestorsielei@gmail.com>
2023-03-05 14:48:13 +0300

    some changes

--- src/main/java/com/USSD/application/controller/UssdApplication.java
+++ src/main/java/com/USSD/application/controller/UssdApplication.java
@@ -1,7 +1,10 @@
 package com.USSD.application.controller;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UssdApplication {
+
+    public static void main(String[] args) {  SpringApplication.run(UssdApplication.class, args);}
 }
