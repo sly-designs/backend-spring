8d1a1d900fa9ea5df5d81a921a09259c325f36a3
slydesigns <sylvestorsielei@gmail.com>
2023-03-05 15:15:58 +0300

    to pick svs from db

--- src/main/java/com/USSD/application/controller/UssdController.java
+++ src/main/java/com/USSD/application/controller/UssdController.java
@@ -1,5 +1,6 @@
 package com.USSD.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,22 +11,24 @@ public class UssdController {
 
     @Autowired
     private ServiceService serviceService;
-    @RequestMapping(value = "/ussd", method = RequestMethod.POST, produces = "text/plain")
-    public String handleUssdRequest(@RequestParam("sessionId") String sessionId,
-                                    @RequestParam("phoneNumber") String phoneNumber,
-                                    @RequestParam("text") String text) {
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    public String ussdCallback(@RequestParam(value = "sessionId", required = true) String sessionId,
+                               @RequestParam(value = "phoneNumber", required = true) String phoneNumber,
+                               @RequestParam(value = "text", required = true) String text) {
+
         // Parse USSD request text to determine current menu state
         String[] parts = text.split("\\*");
         String state = (parts.length > 0) ? parts[parts.length - 1] : "";
 
-        // Route USSD request to appropriate action method
         if (state.equals("")) {
             // Initial menu state
             return "CON Welcome to our booking service.\n" +
                     "Please select an option:\n" +
-                    "1. Book Space\n" +
-                    "2. Check Bookings\n" +
-                    "3. Cancel Bookings";
+                    "1. " + serviceService.getServiceById(1L).getName() + "\n" +
+                    "2. " + serviceService.getServiceById(2L).getName() + "\n" +
+                    "3. " + serviceService.getServiceById(3L).getName();
         } else if (state.equals("1")) {
             // Book Space action
             // TODO: Implement book space logic
@@ -43,26 +46,77 @@ public class UssdController {
                     "3. Booking 3";
         } else if (state.equals("3")) {
             // Cancel Bookings action
-            // TODO: Implement cancel bookings
-
-            return "CON Enter booking ID to cancel:";
-        } else if (state.startsWith("3*")) {
-            // Cancel Bookings action - Enter booking ID
             // TODO: Implement cancel bookings logic
-            return "CON Are you sure you want to cancel this booking?\n" +
-                    "1. Yes\n" +
-                    "2. No";
-        } else if (state.startsWith("3*") && state.endsWith("1")) {
-            // Cancel Bookings action - Confirm cancellation
-            // TODO: Implement cancellation confirmation logic
-            return "END Booking cancelled.";
-        } else if (state.startsWith("3*") && state.endsWith("2")) {
-            // Cancel Bookings action - Cancel cancellation
-            return "END Cancellation cancelled.";
-        } else {
-            // Invalid menu state
-            return "END Invalid input. Please try again.";
+            return "CON Enter booking ID to cancel:";
         }
+
+        // Return default response
+        return "END Invalid input. Please try again.";
     }
+
+
+
+
+
+
+
+
+
+
+
+//    @RequestMapping(value = "/ussd", method = RequestMethod.POST, produces = "text/plain")
+//    public String handleUssdRequest(@RequestParam("sessionId") String sessionId,
+//                                    @RequestParam("phoneNumber") String phoneNumber,
+//                                    @RequestParam("text") String text) {
+//        // Parse USSD request text to determine current menu state
+//        String[] parts = text.split("\\*");
+//        String state = (parts.length > 0) ? parts[parts.length - 1] : "";
+//
+//        // Route USSD request to appropriate action method
+//        if (state.equals("")) {
+//            // Initial menu state
+//            return "CON Welcome to our booking service.\n" +
+//                    "Please select an option:\n" +
+//                    "1. Book Space\n" +
+//                    "2. Check Bookings\n" +
+//                    "3. Cancel Bookings";
+//        } else if (state.equals("1")) {
+//            // Book Space action
+//            // TODO: Implement book space logic
+//            return "CON Enter your name:";
+//        } else if (state.equals("1*name")) {
+//            // Book Space action - Enter name
+//            // TODO: Save user's name and implement other booking details
+//            return "CON Enter your email address:";
+//        } else if (state.equals("2")) {
+//            // Check Bookings action
+//            // TODO: Implement check bookings logic
+//            return "CON Your bookings:\n" +
+//                    "1. Booking 1\n" +
+//                    "2. Booking 2\n" +
+//                    "3. Booking 3";
+//        } else if (state.equals("3")) {
+//            // Cancel Bookings action
+//            // TODO: Implement cancel bookings
+//
+//            return "CON Enter booking ID to cancel:";
+//        } else if (state.startsWith("3*")) {
+//            // Cancel Bookings action - Enter booking ID
+//            // TODO: Implement cancel bookings logic
+//            return "CON Are you sure you want to cancel this booking?\n" +
+//                    "1. Yes\n" +
+//                    "2. No";
+//        } else if (state.startsWith("3*") && state.endsWith("1")) {
+//            // Cancel Bookings action - Confirm cancellation
+//            // TODO: Implement cancellation confirmation logic
+//            return "END Booking cancelled.";
+//        } else if (state.startsWith("3*") && state.endsWith("2")) {
+//            // Cancel Bookings action - Cancel cancellation
+//            return "END Cancellation cancelled.";
+//        } else {
+//            // Invalid menu state
+//            return "END Invalid input. Please try again.";
+//        }
+//    }
 }
 
