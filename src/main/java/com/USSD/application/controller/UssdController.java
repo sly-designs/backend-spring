655218986b320b795bfb08d9026cd493589859b9
slydesigns <sylvestorsielei@gmail.com>
2023-03-05 15:17:06 +0300

    some changes

--- src/main/java/com/USSD/application/controller/UssdController.java
+++ src/main/java/com/USSD/application/controller/UssdController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class UssdController {
