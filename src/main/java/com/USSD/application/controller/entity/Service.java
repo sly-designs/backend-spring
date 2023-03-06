a8417dfc3a7e5f38b1b2f23682340d4950a68838
slydesigns <sylvestorsielei@gmail.com>
2023-03-06 20:28:45 +0300

    changes

--- src/main/java/com/USSD/application/controller/entity/Service.java
+++ src/main/java/com/USSD/application/controller/entity/Service.java
@@ -1,4 +1,13 @@
 package com.USSD.application.controller.entity;
 
 public class Service {
+
+    private Long id;
+
+    private String name;
+    private String description;
+
+    public Service() {
+
+    }
 }
