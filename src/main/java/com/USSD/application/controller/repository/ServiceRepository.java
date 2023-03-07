e637287034c052c86feb742a48b504098a0e9568
slydesigns <sylvestorsielei@gmail.com>
2023-03-07 20:33:38 +0300

    jpa

--- src/main/java/com/USSD/application/controller/repository/ServiceRepository.java
+++ src/main/java/com/USSD/application/controller/repository/ServiceRepository.java
@@ -1,4 +1,6 @@
 package com.USSD.application.controller.repository;
 
-public class ServiceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Long> {
 }
