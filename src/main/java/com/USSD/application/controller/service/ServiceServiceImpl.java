48fd9b5f8d4d190ee46acc3d9be6db7ccdab4448
slydesigns <sylvestorsielei@gmail.com>
2023-03-07 20:21:34 +0300

    getallservices

--- src/main/java/com/USSD/application/controller/service/ServiceServiceImpl.java
+++ src/main/java/com/USSD/application/controller/service/ServiceServiceImpl.java
@@ -1,4 +1,16 @@
 package com.USSD.application.controller.service;
 
-public class ServiceServiceImpl {
+public class ServiceServiceImpl implements ServiceService{
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+    @Override
+    public Service getServiceById(Long id) {
+        return serviceRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Service> getAllServices() {
+        return serviceRepository.findAll();
+    }
 }
